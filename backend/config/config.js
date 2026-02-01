27591ebffb80ca530f1655d0346683144f07807d
cvrafael <caio-victor-rafael@hotmail.com>
2026-01-31 23:10:57 -0300

    :sparkles: Create migrations

---
+++ backend/config/config.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+console.log('teste',process.env.USERNAME)
+module.exports = 
+  {
+    "development": 
+    {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+    "logging": true,
+    },
+    "test": 
+    {
+     "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+    "logging": true,
+    },
+    "production": 
+    {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+    "logging": false,
+    },
+  }
